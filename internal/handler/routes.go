b0da327002c12d2b40041f8c6e246f81d7926a14
Crismary Castellanos <crismary04@gmail.com>
2026-05-18 22:54:51 -0300

    feat(monitoring): tune latency demo traffic (#8)

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -5,6 +5,7 @@ import "github.com/go-chi/chi/v5"
 func RegisterRoutes(r chi.Router, movieHandler *MovieHandler) {
 	r.Get("/health", Health)
 	r.Get("/debug/error", DebugError)
+	r.Get("/debug/slow", DebugSlow)
 	r.HandleFunc("/metrics", Metrics)
 	r.Mount("/movies", movieHandler.Routes())
 }
